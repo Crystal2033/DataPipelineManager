3d01d65a97c305ee78939e975dbb61590309a072
Kulikov Pavel <kulikov-pavlik@bk.ru>
2023-06-08 20:20:37 +0300

    Made connection to database

--- src/main/java/ru/mai/lessons/rpks/ServiceManagmentMain.java
+++ src/main/java/ru/mai/lessons/rpks/ServiceManagmentMain.java
@@ -2,6 +2,8 @@ package ru.mai.lessons.rpks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class ServiceManagmentMain {
