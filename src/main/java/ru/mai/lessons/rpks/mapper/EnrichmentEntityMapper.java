a8bb8da4e1680592ff1b068c55e9af5030655b56
Fogetik <danila041202@gmail.com>
2023-06-20 12:56:57 +0300

    bugs fix

--- src/main/java/ru/mai/lessons/rpks/mapper/EnrichmentEntityMapper.java
+++ src/main/java/ru/mai/lessons/rpks/mapper/EnrichmentEntityMapper.java
@@ -1,27 +1,13 @@
 package ru.mai.lessons.rpks.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.mai.lessons.rpks.database.entity.EnrichmentEntity;
 import ru.mai.lessons.rpks.model.Enrichment;
 
 @Mapper
 public interface EnrichmentEntityMapper {
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "enrichmentId", target = "enrichmentId")
-    @Mapping(source = "ruleId", target = "ruleId")
-    @Mapping(source = "fieldName", target = "fieldName")
-    @Mapping(source = "fieldNameEnrichment", target = "fieldNameEnrichment")
-    @Mapping(source = "fieldValue", target = "fieldValue")
-    @Mapping(source = "fieldValueDefault", target = "fieldValueDefault")
+
     EnrichmentEntity toEnrichmentEntity(Enrichment enrichment);
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "enrichmentId", target = "enrichmentId")
-    @Mapping(source = "ruleId", target = "ruleId")
-    @Mapping(source = "fieldName", target = "fieldName")
-    @Mapping(source = "fieldNameEnrichment", target = "fieldNameEnrichment")
-    @Mapping(source = "fieldValue", target = "fieldValue")
-    @Mapping(source = "fieldValueDefault", target = "fieldValueDefault")
     Enrichment toEnrichment(EnrichmentEntity enrichmentEntity);
 }
